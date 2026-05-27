bc1cdfb2b3b5b3c82904251ffcb8eaa7cd82114b
monikapock <monika.pock@statistik.gv.at>
2026-05-27 14:12:20 +0000

    first draft

---
+++ supervised.py
@@ -0,0 +1,148 @@
+# %%
+
+from dotenv import load_dotenv
+import mlflow
+from sklearn.model_selection import train_test_split
+import polars as pl
+from sklearn.preprocessing import LabelEncoder
+from torchTextClassifiers.value_encoder import ValueEncoder
+from torchTextClassifiers.tokenizers import WordPieceTokenizer
+from torchTextClassifiers import ModelConfig, TrainingConfig, torchTextClassifiers
+
+load_dotenv(override=True)
+
+# %%
+
+# INSERT YOUR CODE - Question #2
+
+# %%
+
+# INSERT YOUR CODE - Question #3
+
+df = pl.read_parquet("https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet")
+
+print(df.head())
+print(f"Total rows: {len(df)}")
+# %%
+n_classes = df['code'].n_unique()
+print(f"Number of unique NACE codes: {n_classes}")
+# %%
+train_df, tmp_df = train_test_split(df, test_size=0.30, random_state=42)
+val_df, test_df  = train_test_split(tmp_df, test_size=0.50, random_state=42)
+
+X_train, y_train = train_df["label"].to_numpy(), train_df["code"].to_numpy()
+X_val, y_val = val_df["label"].to_numpy(), val_df["code"].to_numpy()
+X_test, y_test = test_df["label"].to_numpy(), test_df["code"].to_numpy()
+
+print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
+
+# %%
+encoder = LabelEncoder()
+encoder.fit(train_df['code'].to_numpy())
+all_codes  = set(df['code'])
+train_codes = set(train_df['code'])
+missing = all_codes - train_codes
+
+if missing:
+    print(f"WARNING: {len(missing)} code(s) missing from training set: {missing}")
+else:
+    print(f"OK — all {len(all_codes)} codes appear in the training set.")
+
+value_encoder = ValueEncoder(label_encoder=encoder)
+
+tokenizer = WordPieceTokenizer(vocab_size=5000, output_dim=10)
+tokenizer.train(X_train)
+
+print("Output tensor size:", tokenizer.tokenize(X_train[0]).input_ids.shape)
+print("Vocabulary size:", tokenizer.vocab_size)
+
+# Look at an example of tokenization
+print("Raw text", X_train[0])
+print(
+    "Tokens id:",
+    tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+)
+print(
+    "Tokens:",
+    tokenizer.tokenizer.convert_ids_to_tokens(
+        tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+    )
+)
+
+embedding_dim = 96
+
+model_config = ModelConfig(
+    embedding_dim=embedding_dim,
+    num_classes=n_classes,)
+
+ttc = torchTextClassifiers(
+    tokenizer=tokenizer,
+    model_config=model_config,
+    value_encoder=value_encoder,
+)
+training_config = TrainingConfig(
+    num_epochs=1,
+    batch_size=128,
+    lr=5 * 1e-4,
+    patience_early_stopping=5,
+)
+mlflow.set_experiment("funathon-2026-project2")
+mlflow.pytorch.autolog()
+
+with mlflow.start_run() as run:
+    # This should take approximately 1-2mn
+    ttc.train(
+        X_train,
+        y_train,
+        training_config=training_config,
+        X_val=X_val,
+        y_val=y_val,
+        verbose=True,
+    )
+
+    mlflow.log_artifacts(
+        training_config.save_path,   # local folder produced by ttc.train()
+        artifact_path="model_artifacts",
+    )
+
+
+# %%
+local_dir = mlflow.artifacts.download_artifacts(
+    artifact_uri="s3://projet-funathon/mlflow-artifacts/2/9a5f1b62a991429492b88575fb108f5c/artifacts/model_artifacts/"
+)
+
+# Rebuild the torchTextClassifiers object from the downloaded files
+ttc = torchTextClassifiers.load(local_dir)
+
+ttc.pytorch_model.eval()
+
+# %%
+local_dir = mlflow.artifacts.download_artifacts(
+    artifact_uri="s3://project-funathon/mlflow-artifacts/2/9a5f1b62a991429492b88575fb108f5c/artifacts/model_artifacts/"
+)
+
+# Rebuild the torchTextClassifiers object from the downloaded files
+ttc = torchTextClassifiers.load(local_dir)
+
+ttc.pytorch_model.eval()
+# %%
+
+import s3fs
+
+fs = s3fs.S3FileSystem(
+    anon=True,  # public bucket
+    endpoint_url="https://minio.lab.sspcloud.fr",
+)
+
+local_dir = "./mlflow-artifacts/"
+fs.get(
+    "projet-funathon/diffusion/mlflow-artifacts/",
+    local_dir,
+    recursive=True,
+)
+# Rebuild the torchTextClassifiers object from the downloaded files
+ttc = torchTextClassifiers.load(local_dir)
+
+ttc.pytorch_model.eval()
+
+# %%
